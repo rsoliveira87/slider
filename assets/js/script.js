87c1e385cc98d1c6d3e82bb623c39e0b902ce2c4
Robson Oliveira <rosoliveira@uolinc.com>
2020-01-28 10:15:02 -0300

    Documentação de métodos no script e adicionado os arquivos do dist no gitignore

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,12 @@ class Slider {
         this.bullets = document.querySelectorAll('.bullets-wrapper .bullet');
     }
 
+    /**
+     * Método inicializador da classe
+     *
+     * @memberof Slider
+     * @method onInit
+     */
     onInit() {
         const self = this;
         self.initSlider();
@@ -15,12 +21,24 @@ class Slider {
         self.bulletsAction();
     }
     
+    /**
+     * Método que adiciona a slide fade-in no item ativo
+     *
+     * @memberof Slider
+     * @method addFadeIn
+     */
     addFadeIn() {
         const self = this;
         self.sliderItems[self.count].classList.remove('fade-out');
         self.sliderItems[self.count].classList.add('fade-in');
     }
     
+    /**
+     * Método que remove a classe fade-in
+     *
+     * @memberof Slider
+     * @method removeFadeIn
+     */
     removeFadeIn() {
         const self = this;
 
@@ -33,6 +51,12 @@ class Slider {
         }
     }
     
+    /**
+     * Método que ativa o próximo item do slide
+     *
+     * @memberof Slider
+     * @method moveRight
+     */
     moveRight() {
         const self = this;
 
@@ -48,7 +72,13 @@ class Slider {
         self.addFadeIn();
         self.addActiveBullet();
     }
-    
+
+    /**
+     * Método que ativa o item anterior do slide
+     *
+     * @memberof Slider
+     * @method moveLeft
+     */    
     moveLeft() {
         const self = this;
 
@@ -65,6 +95,12 @@ class Slider {
         self.addActiveBullet();
     }
     
+    /**
+     * Método que inicia o slide automaticamente
+     *
+     * @memberof Slider
+     * @method initSlider
+     */
     initSlider() {
         const self = this;
 
@@ -73,6 +109,12 @@ class Slider {
         }, 3000);
     }
     
+    /**
+     * Método que aplica ação nas setas direita e esquerda
+     *
+     * @memberof Slider
+     * @method arrowsAction
+     */
     arrowsAction() {
         const arrows = document.querySelectorAll('.slider .arrow'), self = this;
 
@@ -93,6 +135,12 @@ class Slider {
         }
     }
 
+    /**
+     * Método que aplica ação nos bullets
+     *
+     * @memberof Slider
+     * @method bulletsAction
+     */
     bulletsAction() {
         const self = this;
 
@@ -108,15 +156,23 @@ class Slider {
 
                 self.count = index;
                 self.removeFadeIn();
-                self.addFadeIn();0
+                self.addFadeIn();
                 self.addActiveBullet();
                 self.initSlider();
             };
         });
     }
 
+    /**
+     * Método que remove a classe active do bullet ativo
+     *
+     * @memberof Slider
+     * @method removeActiveBullet
+     */
     removeActiveBullet() {
-        for(const bullet of this.bullets) {
+        const self = this;
+
+        for(const bullet of self.bullets) {
             if(bullet.classList.contains('active')) {
                 bullet.classList.remove('active');
                 break;
@@ -124,12 +180,19 @@ class Slider {
         }
     }
 
+    /**
+     * Método que adiciona a classe active no bullet ativo
+     *
+     * @memberof Slider
+     * @method addActiveBullet
+     */
     addActiveBullet() {
         const self = this;
         self.bullets[self.count].classList.add('active');
     }
 }
 
+// Instância da classe
 const slider = new Slider;
 
 slider.onInit();
